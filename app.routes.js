daf7dad10c55b48f1be4512079885a7165bf8201
Emily Schmidtling <eyschmidtling@gmail.com>
2016-09-06 12:33:10 -0700

    adds app.routes.js

---
+++ app.routes.js
@@ -0,0 +1,32 @@
+angular
+  .module('CFootprintsApp')
+  .config(AppRoutes)
+
+AppRoutes.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+function AppRoutes($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('homePage', {
+      url: '/',
+      templateUrl: './templates/home.html',
+      controller: 'LoginController',
+      controllerAs: 'loginCtrl'
+    })
+
+    .state('searchPage', {
+      url: '/search',
+      templateUrl: './templates/search.html',
+      controller: 'SearchController',
+      controllerAs: 'searchCtrl'
+    })
+
+    .state('resultsPage', {
+      url: '/results',
+      templateUrl: './templates/results.html',
+      controller: 'ResultsController',
+      controllerAs: 'resultsCtrl'
+    })
+
+    $urlRouterProvider.otherwise('/')
+}
