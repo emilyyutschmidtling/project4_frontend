2d4ad034a3a4ca1f5dccded85b0ac67029088e8d
Emily Schmidtling <eyschmidtling@gmail.com>
2016-09-08 20:59:02 -0700

    changes more http to https

--- js/auth.service.js
+++ js/auth.service.js
@@ -25,7 +25,7 @@
       var promise = $http({
         method: 'POST',
         // url:    '/api/token',
-        url:  'http://carbon-footprints.herokuapp.com/api/token',
+        url:  'https://carbon-footprints.herokuapp.com/api/token',
         // url:  'http://localhost:3000/api/token',
         data:   data,
         headers: {
