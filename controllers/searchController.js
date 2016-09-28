a7254cf16c9ee8662be8fa56a3ea683f94f8f12f
Emily Schmidtling <eyschmidtling@gmail.com>
2016-09-27 23:48:34 -0700

    adds public proxy servers https://cors-anywhere.herokuapp.com and https://crossorigin.me as cors workaround

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -60,15 +60,22 @@ function SearchController($resource, NgMap, $state, $http) {
     console.log("origin:", origin)
     console.log("destination:", destination)
 
-    var driveRouteObject = "https://crossorigin.me/https://maps.googleapis.com/maps/api/directions/json?origin=" + origin +  "&destination=" + destination+ "&key=AIzaSyBLZCoHtlBHXnz1j6iNOmh7H4b2t1Njryc"
+    var driveRouteObject = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?origin=" + origin +  "&destination=" + destination+ "&key=AIzaSyBLZCoHtlBHXnz1j6iNOmh7H4b2t1Njryc"
 
     var transitRouteObject = "https://crossorigin.me/https://maps.googleapis.com/maps/api/directions/json?origin=" + origin +  "&destination=" + destination+ "&mode=transit&key=AIzaSyBLZCoHtlBHXnz1j6iNOmh7H4b2t1Njryc"
+
+    // var driveRouteObject = "https://maps.googleapis.com/maps/api/js/directions/json?origin=" + origin +  "&destination=" + destination+ "&key=AIzaSyBLZCoHtlBHXnz1j6iNOmh7H4b2t1Njryc"
+    //
+    // var transitRouteObject = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin +  "&destination=" + destination+ "&mode=transit&key=AIzaSyBLZCoHtlBHXnz1j6iNOmh7H4b2t1Njryc"
+
 
     console.log("driveRouteObject", driveRouteObject)
     console.log("transitRouteObject", transitRouteObject)
 
     // driveDistance = driveRouteObject
     // driveDistance = driveRouteObject.routes[0].legs[0].distance.text
+
+    delete $http.defaults.headers.common['X-Requested-With'];
 
     $http
       // .get(driveRouteObject, {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }})
@@ -83,6 +90,24 @@ function SearchController($resource, NgMap, $state, $http) {
         vm.drivingCO2 = (numMilesDriving * 0.355).toFixed(3)
         console.log("drivingCO2", vm.drivingCO2)
       })
+
+    // var xhr = new XMLHttpRequest();
+    //
+    // xhr.open('GET', driveRouteObject, true)
+    //
+    // xhr.onload = function(res) {
+    //   console.log("distance:", res.data.routes[0].legs[0].distance.text)
+    //   vm.drivingMiles = res.data.routes[0].legs[0].distance.text
+    //
+    //   var numMilesDriving = vm.drivingMiles.split(" ").shift()
+    //   console.log("numMilesDriving", numMilesDriving)
+    //
+    //   vm.drivingCO2 = (numMilesDriving * 0.355).toFixed(3)
+    //   console.log("drivingCO2", vm.drivingCO2)
+    // }
+    //
+    // xhr.send()
+
 
     $http
       // .get(transitRouteObject, {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }})
